3716015856a9cc2992060a708b59e30c2641cb53
Anna Lukianova <ann.lookyanova@gmail.com>
2020-07-14 15:38:40 +0200

    Initial commit

---
+++ love_my_crypto/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        HELLO
+        <p className="App-link">Go to currencies &rarr;</p>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
