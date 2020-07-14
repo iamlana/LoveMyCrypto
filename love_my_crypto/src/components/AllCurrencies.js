5555b655b85ec9befb070a9b768ffc344a5992df
Anna Lukianova <ann.lookyanova@gmail.com>
2020-07-14 21:07:07 +0200

    Add separate components

---
+++ love_my_crypto/src/components/AllCurrencies.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+ function AllCurrencies() {
+  return (
+    <div className="currencies_list">
+      <header className="App-header">
+        Press ♥ to add your favorite Cryptocurrency to Favorite
+        <ol>
+          <li className="currency">hhhh</li>
+          <li className="currency">gvhc</li>
+          <li className="currency">jkk</li>
+          <li className="currency">hhhh</li>
+          <li className="currency">jjjj</li>
+          <li className="currency"></li>
+          <li className="currency"></li>
+          <li className="currency"></li>
+          <li className="currency"></li>
+          <li className="currency"></li>
+        </ol>
+
+      </header>
+    </div>
+  )
+}
---
+++ love_my_crypto/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        HELLO
+        <button className="App-link">Go to cryptocurrencies</button>
+      </header>
+    </div>
+  )
+}
