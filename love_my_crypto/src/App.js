87e9bd01f660eabf1e29f446e652def535b4cc0c
Anna Lukianova <ann.lookyanova@gmail.com>
2020-07-14 21:07:24 +0200

    Add style to home page

--- love_my_crypto/src/App.js
+++ love_my_crypto/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import {Home} from './components/Home'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        HELLO
-        <p className="App-link">Go to currencies &rarr;</p>
-      </header>
-      
-    </div>
+  <Home/>
   );
 }
 
