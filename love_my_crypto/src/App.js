670918c54b20d6281c72cf33c4f00bab147b0570
Anna Lukianova <ann.lookyanova@gmail.com>
2020-07-14 21:26:44 +0200

    Fix

--- love_my_crypto/src/App.js
+++ love_my_crypto/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-import {Home} from './components/Home'
-
+import { Home } from './components/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-  <Home/>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
